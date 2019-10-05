891a3531a669b389ef13893d603052196ffe6371
Dylan Theriot <dylantheriot@gmail.com>
2019-10-05 07:53:51 -0500

    initial proj

---
+++ TEmp2/Assets/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpHeight;
+    public Transform groundCheck1;
+    public LayerMask groundLayer;
+    public bool isGrounded;
+    public float distance;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = IsGrounded();
+        
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+    }
+
+    bool IsGrounded()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = 3.0f;
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        Debug.DrawRay(position, direction, Color.blue, distance);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
